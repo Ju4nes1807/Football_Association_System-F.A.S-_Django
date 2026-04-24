d8c612bc87d4075eb48ccdaf5acb90220687ec3f
infanterodrigueze277 <infanterodrigueze277@gmail.com>
2026-04-24 10:11:00 -0500

    Errores de tests y seguridad de clave secreta y hosts de envios de emails corregidos

--- f_a_s/settings.py
+++ f_a_s/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,8 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9z436o@f*!4cu%ue!)qs=w=14bkmvw(lz8#-+j!6(78xdwo64v'
+env = environ.Env()
+environ.Env.read_env()
+
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,9 +96,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'footballfas2026@gmail.com'      # ← tu correo
-EMAIL_HOST_PASSWORD = 'hjfy zvnx ksjp kowd'     # ← contraseña de aplicación
-DEFAULT_FROM_EMAIL = 'F.A.S <footballfas2026@gmail.com'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = f"F.A.S <{EMAIL_HOST_USER}>"
 WSGI_APPLICATION = 'f_a_s.wsgi.application'
 
 
